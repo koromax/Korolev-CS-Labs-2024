f9de09cf2abeb2fa7e7071c522973c7dcf06005f
koromax <49192068+koromax@users.noreply.github.com>
2024-09-16 14:41:22 +0300

    politeness update

--- main.cpp
+++ main.cpp
@@ -1,90 +1,102 @@
 #include <iostream>
 
 namespace {
-const int minAllowedValue = 0;
-const int maxAllowedHours = 23;
-const int maxAllowedMinutes = 59;
-const int night = 0;
-const int morning = 5;
-const int noon = 12;
-const int evening = 18;
-const int dayCycleLength = 24;
+const int kMinAllowedValueOfHours = 0;
+const int kMaxAllowedValueOfHours = 23;
+const int kMinAllowedValueOfMinutes = 0;
+const int kMaxAllowedValueOfMinutes = 59;
 
-const int hoursOVRuleException = 1;
-const int hoursARuleLBound = 2;
-const int hoursARuleRBound = 4;
+const int kHourWhenNightBegins = 0;
+const int kHourWhenNightEnds = 5;
+const int kHourWhenMorningBegins = 5;
+const int kHourWhenMorningEnds = 12;
+const int kHourWhenDaytimeBegins = 12;
+const int kHourWhenDaytimeEnds = 18;
+const int kHourWhenEveningBegins = 18;
+const int kHourWhenEveningEnds = 24;
 
-const int minutesARule = 1;
-const int minutesARuleException = 11;
-const int minutesUIRuleLBound = 2;
-const int minutesUIRuleRBound = 4;
-const int minutesUIRuleExceptionLBound = 10;
-const int minutesUIRuleExceptionRBound = 20;
-const int divisorToDetermineTheSecondDigit = 10;
+const int kDivisorToDetermineTheLastDigitOfANumberInBase10 = 10;
+const int kLastHourWhenNoSubtractionIsNeeded = 12;
+const int kAmountOfHoursToSubtract = 12;
+
+const int kHourIsExact = 0;
+const int kMidnightHour = 0;
+const int kNoonHour = 12;
+
+const int kHoursGenitiveCaseException = 1;
+const int kHoursGenitiveCaseLowerLimit = 2;
+const int kHoursGenitiveCaseUpperLimit = 4;
+
+const int kMinutesNominativeCaseCriteria = 1;
+const int kMinutesNominativeCaseException = 11;
+const int kMinutesGenitiveCaseLowerLimit = 2;
+const int kMinutesGenitiveCaseUpperLimit = 4;
+const int kMinutesGenitiveCaseExceptionLowerLimit = 10;
+const int kMinutesGenitiveCaseExceptionUpperLimit = 20;
 }  // namespace
 
 int main(int, char**) {
+    std::cout << "Введите время в 24-часовом формате: ";
     int hours = 0;
     int minutes = 0;
     std::cin >> hours >> minutes;
 
-    int hours_12hFormat = (hours > noon) ? (hours - noon) : hours;
-    int minutesSecondDigit = minutes % divisorToDetermineTheSecondDigit;
+    int hours12hFormat = (hours > kLastHourWhenNoSubtractionIsNeeded) ? (hours - kAmountOfHoursToSubtract) : hours;
+    int minutesSecondDigit = minutes % kDivisorToDetermineTheLastDigitOfANumberInBase10;
 
-    if (hours < minAllowedValue or maxAllowedHours < hours) {
-        std::cout << "Часов больше чем надо\n";
-        return 0;
-    }
-    if (minutes < minAllowedValue or maxAllowedMinutes < minutes) {
-        std::cout << "Минут больше чем надо\n";
-        return 0;
+    if ((hours < kMinAllowedValueOfHours || hours > kMaxAllowedValueOfHours) ||
+        (minutes < kMinAllowedValueOfMinutes || minutes > kMaxAllowedValueOfMinutes)) {
+        std::cout << "неверные данные\n";
+        return 1;
     }
 
-    if (minutes == night) {
-        if (hours == night or hours == dayCycleLength) {
+    if (minutes == kHourIsExact) {
+        if (hours == kMidnightHour) {
             std::cout << "полночь\n";
             return 0;
         }
-        if (hours == noon) {
+        if (hours == kNoonHour) {
             std::cout << "полдень\n";
             return 0;
         }
     }
 
-    std::cout << hours_12hFormat << " ";
-    std::cout << "час";
-    if ((hoursARuleLBound <= hours_12hFormat and hours_12hFormat <= hoursARuleRBound)) {
-        std::cout << "а";
-    } else if (hours_12hFormat != hoursOVRuleException) {
-        std::cout << "ов";
+    std::cout << hours12hFormat << " ";
+    if ((kHoursGenitiveCaseLowerLimit <= hours12hFormat && kHoursGenitiveCaseUpperLimit >= hours12hFormat)) {
+        std::cout << "часа";
+    } else if (hours12hFormat == kHoursGenitiveCaseException) {
+        std::cout << "час";
+    } else {
+        std::cout << "часов";
     }
 
-    if (minutes != 0) {
-        std::cout << " " << minutes << " минут";
+    if (minutes != kHourIsExact) {
+        std::cout << " " << minutes << " ";
     }
 
-    if (minutesSecondDigit == minutesARule and minutes != minutesARuleException) {
-        std::cout << "а";
-    } else if ((minutesUIRuleLBound <= minutesSecondDigit and minutesSecondDigit <= minutesUIRuleRBound) and
-               (minutes < minutesUIRuleExceptionLBound or minutes > minutesUIRuleExceptionRBound)) {
-        std::cout << "ы";
+    if (minutesSecondDigit == kMinutesNominativeCaseCriteria && minutes != kMinutesNominativeCaseException) {
+        std::cout << "минута";
+    } else if ((minutesSecondDigit >= kMinutesGenitiveCaseLowerLimit && minutesSecondDigit <= kMinutesGenitiveCaseUpperLimit) &&
+               (minutes < kMinutesGenitiveCaseExceptionLowerLimit || minutes > kMinutesGenitiveCaseExceptionUpperLimit)) {
+        std::cout << "минуты";
+    } else if (minutes != kHourIsExact) {
+        std::cout << "минут";
     }
 
-    if (night <= hours and hours < morning) {
+    if (kHourWhenNightBegins <= hours && kHourWhenNightEnds > hours) {
         std::cout << " ночи";
-    } else if (morning <= hours and hours < noon) {
+    } else if (kHourWhenMorningBegins <= hours && kHourWhenMorningEnds > hours) {
         std::cout << " утра";
-    } else if (noon <= hours and hours < evening) {
+    } else if (kHourWhenDaytimeBegins <= hours && kHourWhenDaytimeEnds > hours) {
         std::cout << " дня";
-    } else if (evening <= hours and hours < dayCycleLength) {
+    } else if (kHourWhenEveningBegins <= hours && kHourWhenEveningEnds > hours) {
         std::cout << " вечера";
     }
 
-    if (minutes == 0) {
-        std::cout << " ровно\n";
-    } else {
-        std::cout << "\n";
+    if (minutes == kHourIsExact) {
+        std::cout << " ровно";
     }
+    std::cout << "\n";
 
     return 0;
 }
